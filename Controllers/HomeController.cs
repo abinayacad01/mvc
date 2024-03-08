6665d2a585884c2c573c45ac7c3daa0c5a78de6f
abinayacad01 <abinayakumarcad01@gmail.com>
2024-03-08 17:14:20 +0530

    adding files

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using mvc.Models;
+
+namespace mvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+   public IActionResult Tutorials()
+    {
+        return View();
+    } 
+    public IActionResult Testpage()
+    {
+        return View();
+    } 
+    public IActionResult Paidcourses()
+    {
+        return View();
+    } 
+
+        public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
