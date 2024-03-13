d3b591d9340312cf0d905d900dc1a1bacd6c2b25
abinayacad01 <abinayakumarcad01@gmail.com>
2024-03-13 17:42:31 +0530

    modified

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using mvc.Models;
+using microsoft.data.sqlclient;
+
+
 
 namespace mvc.Controllers;
 
 public class HomeController : Controller
 {
+    SqlConnection con = new sqlConnection();
+    SqlConnection com = new sqlCommand();
+    SqlDataReader? dr;
+
     private readonly ILogger<HomeController> _logger;
 
     public HomeController(ILogger<HomeController> logger)
@@ -30,9 +37,37 @@ public class HomeController : Controller
     {
         return View();
     } 
+
+[httpGet]
+
     public IActionResult Login()
     {
         return View();
+    }
+
+    void connectionstring()
+    {
+        con.connectionstring = "datasource= 192.168.1.240\SQLEXPRESS; database= cad_lw; integrated security=SSPI; TrustServiceCertificate= True;";
+
+    }
+
+    [httpGet]
+
+    public IActionResult verifylogin(loginmodel lmodel)
+    {
+        connectionstring();
+        con.open();
+        com.CommandText= "select * from USER_LOGIN where USER_ID = '"+lmodel.username+"' and '"+lmodel.password+"';";
+
+        dr=com.ExecuteReader();
+        if(dr.Read()){
+            con.Close();
+            return View("create");
+        }
+        else{
+            con.Close();
+            return View("error");
+        }
     }
         public IActionResult AboutUs()
     {
---
+++ Models/loginmodel.cs
@@ -0,0 +1,8 @@
+namespace mvc.Models;
+
+public class loginModel
+{
+    public string? username { get; set; }
+    public string? password { get; set; }
+
+}
